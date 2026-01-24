8ef07bddb4d37f937f71951de7f3b0a4cb4100a1
Harshavardhan Vadluri <iiharsha10@gmail.com>
2026-01-24 13:10:34 +0530

    changed a lil bit of the template

--- template.h
+++ template.h
@@ -1,5 +1,5 @@
-#ifndef TEMPLATE_H
-#define TEMPLATE_H
+#ifndef DSACPP_TEMPLATE_H
+#define DSACPP_TEMPLATE_H
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,4 +7,4 @@ using namespace std;
   ios::sync_with_stdio(false);                                                 \
   cin.tie(NULL);
 
-#endif // "TEMPLATE_H"
+#endif // DSACPP_TEMPLATE_H
